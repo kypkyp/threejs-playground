30d6b1c8c17bceb40a04faf731a8f2fbada36095
kyp <kyp@kmc.gr.jp>
2020-11-07 23:45:23 +0900

    光源と影を設定

--- pages/dirksen/1/1.vue
+++ pages/dirksen/1/1.vue
@@ -20,44 +20,54 @@ export default {
     renderer.setClearColor(new THREE.Color(0xeeeeee));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
     const axes = new THREE.AxesHelper(20);
     scene.add(axes);
 
+    const spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-20, 30, -5);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+    scene.add(spotLight);
+
     const planeGeometry = new THREE.PlaneGeometry(60, 20);
-    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.x = 15;
     plane.position.y = 0;
     plane.position.z = 0;
+    plane.receiveShadow = true;
 
     scene.add(plane);
 
     const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    const cubeMaterial = new THREE.MeshBasicMaterial({
+    const cubeMaterial = new THREE.MeshLambertMaterial({
       color: 0xff0000,
-      wireframe: true,
     });
     const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
     cube.position.x = -4;
     cube.position.y = 3;
     cube.position.z = 0;
+    cube.castShadow = true;
 
     scene.add(cube);
 
     const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    const sphereMaterial = new THREE.MeshBasicMaterial({
+    const sphereMaterial = new THREE.MeshLambertMaterial({
       color: 0x7777ff,
-      wireframe: true,
     });
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
     sphere.position.x = 20;
     sphere.position.y = 4;
     sphere.position.z = 2;
+    sphere.castShadow = true;
 
     scene.add(sphere);
 
